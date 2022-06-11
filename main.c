28216370c7203b50ac124c99ac3cdf5b16fefcab
Steven Odhiambo <60733003+Sephens@users.noreply.github.com>
2022-06-11 23:48:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -37,8 +37,7 @@ void set_data(data_shell *datash, char **av)
 	datash->counter = 1;
 
 	for (i = 0; environ[i]; i++)
-		;
-
+	
 	datash->_environ = malloc(sizeof(char *) * (i + 1));
 
 	for (i = 0; environ[i]; i++)
