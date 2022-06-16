e6bff8fb7d7ae20ad267b3eea077ff9e09b31c75
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:01:31 +0300

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * bring_line - assigns the line var for get_line
- * @lineptr: Buffer that store the input str
- * @buffer: str that is been called to line
- * @n: size of line
- * @j: size of buffer
+ * bring_line - this assigns the line var for get_line
+ * @lineptr: Buffer that stores the input str
+ * @buffer: str that is being called to line
+ * @n: size of the line
+ * @j: size of the buffer
  */
 void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 {
