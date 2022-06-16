f34209c062678aa54f9f550cb8fc3246ef91c662
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-16 23:54:28 +0300

    Update rep_var.c

--- rep_var.c
+++ rep_var.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * check_env - checks if the typed variable is an env variable
+ * check_env - check if the typed variable is an env variable
  *
  * @h: head of linked list
- * @in: input string
- * @data: data structure
+ * @in: the input string
+ * @data: the data structure
  * Return: no return
  */
 void check_env(r_var **h, char *in, data_shell *data)
@@ -45,9 +45,9 @@ void check_env(r_var **h, char *in, data_shell *data)
  * check_vars - check if the typed variable is $$ or $?
  *
  * @h: head of the linked list
- * @in: input string
- * @st: last status of the Shell
- * @data: data structure
+ * @in: the input string
+ * @st: the last status of the Shell
+ * @data: the data structure
  * Return: no return
  */
 int check_vars(r_var **h, char *in, char *st, data_shell *data)
@@ -87,7 +87,7 @@ int check_vars(r_var **h, char *in, char *st, data_shell *data)
  * replaced_input - replaces string into variables
  *
  * @head: head of the linked list
- * @input: input string
+ * @input: the input string
  * @new_input: new input string (replaced)
  * @nlen: new length
  * Return: replaced string
@@ -138,8 +138,8 @@ char *replaced_input(r_var **head, char *input, char *new_input, int nlen)
 /**
  * rep_var - calls functions to replace string into vars
  *
- * @input: input string
- * @datash: data structure
+ * @input: the input string
+ * @datash: the data structure
  * Return: replaced string
  */
 char *rep_var(char *input, data_shell *datash)
