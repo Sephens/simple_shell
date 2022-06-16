dec552c469052fae809efc87ce3a15b3c5e04537
Apple <93719602+wangui-ann@users.noreply.github.com>
2022-06-17 00:00:39 +0300

    Update get_sigint.c

--- get_sigint.c
+++ get_sigint.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * get_sigint - Handle the crtl + c call in prompt
+ * get_sigint - Handles the crtl + c call in prompt
  * @sig: Signal handler
  */
 void get_sigint(int sig)
